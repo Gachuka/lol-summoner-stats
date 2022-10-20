3e12e598c7f8a1af9a832b92938b01c508cb4911
Li Yu <gachuka@gmail.com>
2022-10-20 09:29:17 -0400

    Basic setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.scss';
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
+import SummonerStatComponent from './components/SummonerStatComponent/SummonerStatComponent';
+
 function App() {
   return (
     <div className="App">
-      
+      <SummonerStatComponent />
     </div>
   );
 }
---
+++ src/components/SummonerStatComponent/SummonerStatComponent.js
@@ -0,0 +1,13 @@
+import './SummonerStatComponent.scss'
+
+function SummonerStatComponent() {
+  return (
+    <div className='ssc'>
+      <div className='ssc__container'>
+        <h1>SUMMONER NAME</h1>
+      </div>
+    </div>
+  )
+}
+
+export default SummonerStatComponent
