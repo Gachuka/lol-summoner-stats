2ff7b421a59f38f4676f8e1e3f7427bc0dff8c6c
Li Yu <gachuka@gmail.com>
2022-10-20 17:34:07 -0400

    Started laying out what needs to be displayed

--- src/components/SummonerStatComponent/SummonerStatComponent.js
+++ src/components/SummonerStatComponent/SummonerStatComponent.js
@@ -1,5 +1,5 @@
 import './SummonerStatComponent.scss'
-import {getSummonerByName,getMatchHistoryByPUUID,getMatchByMatchId} from '../../utilities/utilities.js'
+import {getSummonerByName,getSummonerByPUUID,getMatchHistoryByPUUID,getMatchByMatchId} from '../../utilities/utilities.js'
 
 import {useEffect, useState} from 'react'
 import {useParams} from 'react-router-dom'
@@ -9,6 +9,7 @@ function SummonerStatComponent() {
   const {summonerName} = useParams()
   const [ summonerData, setSummonerData ] = useState()
   const [ matchHistoryData, setMatchHistoryData ] = useState()
+  const [ participants, setParticipants ] = useState([])
 
   useEffect(() => {
     getSummonerByName(summonerName).then((res) => {
@@ -22,21 +23,45 @@ function SummonerStatComponent() {
     if(summonerData) {
       getMatchHistoryByPUUID(summonerData['puuid']).then((res) => {
         setMatchHistoryData(res.data)
+        // console.log(res.data)
       })
     }
   },[summonerData])
   
+  // useEffect(() => {
+  //   if(matchHistoryData) {
+  //     getMatchByMatchId(matchHistoryData[0]).then((res) => {
+  //       // console.log(res.data.metadata.participants)
+  //       const matchParticipants = res.data.metadata.participants
+  //       const matchSummonerNames = []
+  //       for ( let i = 0; i < matchParticipants.length; i++) {
+  //         getSummonerByPUUID(matchParticipants[i]).then((res) => {
+  //           matchSummonerNames.push(res.data.name)
+  //         })
+  //       }
+  //       console.log(matchSummonerNames)
+  //     })
+  //   }
+  // },[matchHistoryData])
   useEffect(() => {
     if(matchHistoryData) {
       getMatchByMatchId(matchHistoryData[0]).then((res) => {
-        console.log(res.data.metadata.participants)
-        const matchParticipants = res.data.metadata.participants
-        console.log(matchParticipants)
+        // console.log(res.data)
+        const participants = res.data.info.participants
+        setParticipants(res.data.info.participants)
+        for(let i = 0; i < participants.length; i ++) {
+          console.log(participants[i].summonerName)
+          console.log(participants[i].championId)
+        }
       })
     }
   },[matchHistoryData])
 
-  if (!summonerData) {
+  const handleRefresh = () => {
+    window.location.reload()
+  }
+
+  if (!summonerData || participants.length === 0) {
     return (
       <h1>Loading</h1>
     )
@@ -46,8 +71,21 @@ function SummonerStatComponent() {
     <div className='ssc'>
       <div className='ssc__container'>
         <h1>{summonerData['name']}</h1>
-        <button>Refresh</button>
-        <div></div>
+        <button onClick={handleRefresh}>Refresh</button>
+        <div className='team2'>
+          <div className='team1-summoner1'>{participants[0].summonerName}</div>
+          <div className='team1-summoner2'>{participants[1].summonerName}</div>
+          <div className='team1-summoner3'>{participants[2].summonerName}</div>
+          <div className='team1-summoner4'>{participants[3].summonerName}</div>
+          <div className='team1-summoner5'>{participants[4].summonerName}</div>
+        </div>
+        <div className='team2'>
+          <div className='team2-summoner1'>{participants[5].summonerName}</div>
+          <div className='team2-summoner2'>{participants[6].summonerName}</div>
+          <div className='team2-summoner3'>{participants[7].summonerName}</div>
+          <div className='team2-summoner4'>{participants[8].summonerName}</div>
+          <div className='team2-summoner5'>{participants[9].summonerName}</div>
+        </div>
       </div>
     </div>
   )
--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const apiKey = 'api_key=RGAPI-326fae6d-e46e-4354-a69c-2c59bc6b2398'
+const apiKey = 'api_key=RGAPI-2219e73f-a3ae-4ab7-8375-1858981bfd1a'
 const apiSummonerUrl = 'https://na1.api.riotgames.com/'
 const apiMatchHistoryUrl = 'https://americas.api.riotgames.com/'
 const requestSummonerBySummonerName = 'lol/summoner/v4/summoners/by-name/'
@@ -13,9 +13,10 @@ export const getSummonerByName = (summonerName) => {
   return axios.get(`${apiSummonerUrl}${requestSummonerBySummonerName}${summonerName}?${apiKey}`)
 }
 
-export const getSummonerByPUUID = (summonerPUUID) => {
-  return axios.get(`${apiSummonerUrl}${requestSummonerByPUUID}${summonerPUUID}?${apiKey}`)
-}
+// export const getSummonerByPUUID = (summonerPUUID) => {
+//   console.log(summonerPUUID)
+//   return axios.get(`${apiSummonerUrl}${requestSummonerByPUUID}${summonerPUUID}?${apiKey}`)
+// }
 
 export const getMatchHistoryByPUUID = (PUUID) => {
   return axios.get(`${apiMatchHistoryUrl}${requestMatchByPUUIDlol}${PUUID}/ids?start=0&count=20&${apiKey}`)
@@ -24,3 +25,5 @@ export const getMatchHistoryByPUUID = (PUUID) => {
 export const getMatchByMatchId = (MatchId) => {
   return axios.get(`${apiMatchHistoryUrl}${requestMatchByMatchId}${MatchId}?${apiKey}`)
 }
+
+// https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/DnYf9rH7-Z30KSFMavGWZiyZqAXgvBXB0gkr288PZVux2TQxRWfAGzQ8Z1xrsTus-yFyTcZBGdKqsA?api_key=RGAPI-326fae6d-e46e-4354-a69c-2c59bc6b2398
