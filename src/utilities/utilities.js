5f62731a2e8f4f48e415af2efae5f68e859c7f7c
Li Yu <gachuka@gmail.com>
2022-11-08 13:51:47 -0500

    Add .env for api_key and fixed FiddleSticks summoner icon

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const apiKey = 'api_key=RGAPI-2c0ba8d2-d840-4a83-9b3f-c7fb4d6a710b'
+const API_KEY = process.env.REACT_APP_API_KEY
+
 const apiSummonerUrl = 'https://na1.api.riotgames.com/'
 const apiMatchHistoryUrl = 'https://americas.api.riotgames.com/'
 const requestSummonerBySummonerName = 'lol/summoner/v4/summoners/by-name/'
@@ -10,20 +11,20 @@ const requestMatchByMatchId = 'lol/match/v5/matches/'
 
 
 export const getSummonerByName = (summonerName) => {
-  return axios.get(`${apiSummonerUrl}${requestSummonerBySummonerName}${summonerName}?${apiKey}`)
+  return axios.get(`${apiSummonerUrl}${requestSummonerBySummonerName}${summonerName}?api_key=${API_KEY}`)
 }
 
 // export const getSummonerByPUUID = (summonerPUUID) => {
 //   console.log(summonerPUUID)
-//   return axios.get(`${apiSummonerUrl}${requestSummonerByPUUID}${summonerPUUID}?${apiKey}`)
+//   return axios.get(`${apiSummonerUrl}${requestSummonerByPUUID}${summonerPUUID}?api_key=${API_KEY}`)
 // }
 
 export const getMatchHistoryByPUUID = (PUUID) => {
-  return axios.get(`${apiMatchHistoryUrl}${requestMatchByPUUIDlol}${PUUID}/ids?start=0&count=50&${apiKey}`)
+  return axios.get(`${apiMatchHistoryUrl}${requestMatchByPUUIDlol}${PUUID}/ids?start=0&count=50&api_key=${API_KEY}`)
 }
 
 export const getMatchByMatchId = (MatchId) => {
-  return axios.get(`${apiMatchHistoryUrl}${requestMatchByMatchId}${MatchId}?${apiKey}`)
+  return axios.get(`${apiMatchHistoryUrl}${requestMatchByMatchId}${MatchId}?api_key=${API_KEY}`)
 }
 
 // https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/DnYf9rH7-Z30KSFMavGWZiyZqAXgvBXB0gkr288PZVux2TQxRWfAGzQ8Z1xrsTus-yFyTcZBGdKqsA?api_key=RGAPI-326fae6d-e46e-4354-a69c-2c59bc6b2398
