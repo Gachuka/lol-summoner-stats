b79a7d886be4bcc99798eb11aa2ea8e8b70f3901
Li Yu <gachuka@gmail.com>
2023-01-20 16:46:27 -0500

    Fix player win or lose icon highlight

--- src/components/ParticipantsComponent/ParticipantsComponent.js
+++ src/components/ParticipantsComponent/ParticipantsComponent.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function ParticipantsComponent({ championName, participantName, summonerName, versionNumber, win }) {
   return (
     <Link to={`/summoner/${participantName}`} className='team'>
-      <div className={`${participantName === summonerName ? "team__champion--player" : "team__champion"} ${participantName === summonerName && win ? "player-win" : "player-lose"}`}>
+      <div className={`${participantName === summonerName ? "team__champion--player" : "team__champion"} ${participantName === summonerName ? win ? "player-win" : "player-lose" : ""}`}>
         <img 
           className='team__champion-icon' 
           src={`http://ddragon.leagueoflegends.com/cdn/${versionNumber}/img/champion/${championName === "FiddleSticks" ? "Fiddlesticks": championName}.png`} 
