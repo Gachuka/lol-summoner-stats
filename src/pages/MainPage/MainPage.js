fa75e747c7401f57e22ff37f270a9a2332bd081d
Li Yu <gachuka@gmail.com>
2023-01-23 10:54:52 -0500

    Add quick background

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,11 +2,29 @@ import './MainPage.scss'
 
 import SearchBarComponent from '../../components/SearchBarComponent/SearchBarComponent'
 
+import MainBackground from '../../assets/background-images/main-background.jpg'
+
 function MainPage() {
 
   return (
     <div className='main'>
       <div className='main__container'>
+
+        <div className='background'>
+          <div className='background__section'>
+            <div className='background__container'>
+              <img
+                className='background__image'
+                src={MainBackground}
+                alt="background recent most used champ"
+              />
+            </div>
+            <div className='background__gradiant'>
+              <div className='background__gradiant-filter'></div>
+            </div>
+          </div>
+        </div>
+
         <h1 className='main__header'><span>10</span>Latest</h1>
         <SearchBarComponent />
       </div>
