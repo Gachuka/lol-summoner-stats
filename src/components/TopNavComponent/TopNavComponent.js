ea0897bc9a95976dca275aadd0b947ca07249baa
Li Yu <gachuka@gmail.com>
2023-01-24 10:36:16 -0500

    Change cursor to pointer on Logo

--- src/components/TopNavComponent/TopNavComponent.js
+++ src/components/TopNavComponent/TopNavComponent.js
@@ -1,14 +1,23 @@
-import SearchBarComponent from '../SearchBarComponent/SearchBarComponent'
 import './TopNavComponent.scss'
 
+import { useNavigate } from 'react-router'
+
+import SearchBarComponent from '../SearchBarComponent/SearchBarComponent'
+
 function TopNavComponent() {
+  
+  const navigate = useNavigate()
 
   const isSummonerPage = (window.location.href.indexOf('summoner') > -1 ? true : false)
+
+  const handleLogoCLick = () => {
+    navigate('/')
+  }
 
   return (
     <nav className='navbar'>
       <div className='navbar__container'>
-        <div className='navbar__logo'><span>10</span>Latest</div>
+        <div className='navbar__logo' onClick={handleLogoCLick}><span>10</span>Latest</div>
         {isSummonerPage && <SearchBarComponent />}
       </div>
     </nav>
